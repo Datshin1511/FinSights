03b6a6dc63094510acb723c5c6a5b2807b1959eb
Datshin1511 <datshinr2002@gmail.com>
2024-09-28 23:03:29 +0530

    FB authentication routes created

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from 'express'
 import session from 'express-session'
 import dotenv from 'dotenv'
 import passport from 'passport'
-import pg from 'passport-google-oauth2'
+import google from 'passport-google-oauth2'
+import facebook from 'passport-facebook'
 import cors from 'cors'
 
 import pool from './db/conn.js'
@@ -10,13 +11,14 @@ import pool from './db/conn.js'
 dotenv.config()
 
 const app = express()
-const OAuth2Strategy = pg.Strategy;
+const OAuth2Strategy4Google = google.Strategy;
+const OAuth2Strategy4Facebook = facebook.Strategy;
 const PORT = process.env.PORT || 3000
 
 // EXPRESS UTILITIES
 
 app.use(cors({
-    origin: `http://localhost:${process.env.PORT}`,
+    origin: `http://localhost:${PORT}`,
     methods: 'GET, POST, PUT, DELETE',
     credentials: true
 }))
@@ -34,10 +36,35 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 passport.use(
-    new OAuth2Strategy({
+    new OAuth2Strategy4Google({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/callback",
+        scope: ["profile","email"]
+    },
+    async(accessToken,refreshToken,profile,done)=>{
+        try {
+            console.log("Profile details: ", profile)
+            
+            // Sample MySQL connection check check
+            const [rows] = await pool.query('SELECT * FROM companies')
+
+            console.log(rows)
+
+            return done(null, rows)
+
+        } catch (error) {
+            return done(error,null)
+        }
+    }
+    )
+)
+
+passport.use(
+    new OAuth2Strategy4Facebook({
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: "/auth/facebook/callback",
         scope: ["profile","email"]
     },
     async(accessToken,refreshToken,profile,done)=>{
@@ -68,21 +95,28 @@ passport.deserializeUser((user, done)=>{
 
 // ROUTES
 
-app.get("/auth/google",passport.authenticate("google",{scope:["profile","email"]}));
+app.get("/auth/google", passport.authenticate("google",{scope:["profile","email"]}));
 
 app.get("/auth/google/callback",passport.authenticate("google",{
     successRedirect:"http://localhost:3000/dashboard",
     failureRedirect:"http://localhost:3000/login"
 }))
 
-app.get("/login/sucess",async(req,res)=>{
+app.get("/auth/facebook", passport.authenticate("facebook",{scope:["profile","email"]}))
 
-    if(req.user){
-        res.status(200).json({message:"User Login",user:req.user})
-    }else{
-        res.status(400).json({message:"Not Authorized"})
-    }
-})
+app.get("/auth/facebook/callback",passport.authenticate("facebook",{
+    successRedirect:"http://localhost:3000/dashboard",
+    failureRedirect:"http://localhost:3000/login"
+}))
+
+// app.get("/login/sucess",async(req,res)=>{
+
+//     if(req.user){
+//         res.status(200).json({message:"User Login",user:req.user})
+//     }else{
+//         res.status(400).json({message:"Not Authorized"})
+//     }
+// })
 
 app.get("/logout",(req,res,next)=>{
     req.logout(function(err){
