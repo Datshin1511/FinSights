a4723712180de82db159796e2f48679b597a3a90
Datshin1511 <datshinr2002@gmail.com>
2024-10-02 12:13:58 +0530

    Error page changed

--- frontend/src/components/Error.js
+++ frontend/src/components/Error.js
@@ -3,7 +3,9 @@ import '../styles/error.css'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='error-page'>
+      <h4 className='fst-bold'> Page under construction or not defined.</h4>
+    </div>
   )
 }
 
