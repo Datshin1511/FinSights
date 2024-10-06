6222008f7e5994d9028442ac59fefeb982cb6568
Datshin1511 <datshinr2002@gmail.com>
2024-10-07 03:46:30 +0530

    Minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import session from 'express-session'
 import cookieParser from 'cookie-parser'
+import passport from 'passport'
 import cors from 'cors'
 import dotenv from 'dotenv'
 
@@ -30,6 +31,9 @@ app.use(session({
     resave: false,
     saveUninitialized: true
 }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes setup
 app.use('/user', userRouter)
